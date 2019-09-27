7a06e4a6783b2c5219680b62f34c36e8d6d1989d
LeilaoMi <55868083+LeilaoMi@users.noreply.github.com>
2019-09-27 14:15:53 +0800

    Update jump7.py

--- jump7.py
+++ jump7.py
@@ -6,3 +6,11 @@ for a in range(1,101):
     if a//10== 7:
         continue
     print(a)
+for a in range(1,101):
+    if a%7== 0:
+        continue
+    elif a%10== 7:
+        continue
+    elif a//10== 7:
+        continue
+    print(a)
